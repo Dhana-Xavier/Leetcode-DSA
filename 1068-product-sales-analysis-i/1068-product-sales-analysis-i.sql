c8b269e5c81db2611caa4c0ca2ad9206067e3c41
Dhana-Xavier <dhanax14@gmail.com>
2026-07-01 20:56:15 +0530

    Time: 1243 ms (63.79%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
-SELECT p.product_name ,s.year , s.price
-from sales s join product p 
-on s.product_id=p.product_id ;
+select p.product_name ,s.year,s.price 
+from sales s join product p
+on s.product_id=p.product_id;
